0a34f17999bb1de0afdd84c7a026782657eb4ef6
Samuel Caetite <srcaetite@gmail.com>
2025-05-29 22:31:16 -0300

    feat: use neobutton component

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,19 @@
-<script setup lang="ts">
-</script>
-
 <template>
+  <main class="app">
+    <NeButton>Click me</NeButton>
+  </main>
 </template>
 
-<style scoped></style>
+<script setup lang="ts">
+import NeButton from './components/NeButton.vue'
+</script>
+
+<style scoped lang="scss">
+.app {
+  min-height: 100vh;
+  display: grid;
+  place-items: center;
+  background-color: #dde1e7;
+  font-family: 'Segoe UI', sans-serif;
+}
+</style>
