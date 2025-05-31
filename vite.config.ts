8bd3dcddbcaad6bb1c8b711ed3c0ebb7841a95f2
Samuel Caetite <srcaetite@gmail.com>
2025-05-31 09:30:58 -0300

    build: configure project to make available package components

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,13 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import path from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   build: {
     lib: {
-      entry: path.resolve(__dirname, "src/index.ts"),
+      entry: "src/index.ts",
       name: "SamorphicVue",
       fileName: (format) => `index.${format}.js`,
-      formats: ["es", "cjs"],
     },
     rollupOptions: {
       external: ["vue"],
