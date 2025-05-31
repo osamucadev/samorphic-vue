1a1da7abfa02f3c5a17125b2757eed182405d494
Samuel Caetite <srcaetite@gmail.com>
2025-05-31 15:57:45 -0300

    feat: add NeButton story using Storybook for Vue 3

---
+++ src/stories/NeButton.stories.ts
@@ -0,0 +1,21 @@
+import type { Meta, StoryObj } from "@storybook/vue3";
+import NeButton from "../components/NeButton.vue";
+
+const meta: Meta<typeof NeButton> = {
+  title: "Components/NeButton",
+  component: NeButton,
+  tags: ["autodocs"],
+  args: {
+    default: "Click Me",
+  },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof NeButton>;
+
+export const Primary: Story = {
+  args: {
+    default: "Click Me",
+  },
+};
