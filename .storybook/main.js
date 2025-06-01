26c6e090d83992ea0695120d41649321e74fde5f
Samuel Caetite <srcaetite@gmail.com>
2025-06-01 07:15:26 -0300

    feat: remove old project files

--- .storybook/main.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  stories: ['../src/**/*.stories.@(js|ts|jsx|tsx|mdx)'],
-  addons: ['@storybook/addon-essentials'],
-  framework: {
-    name: '@storybook/vue3-vite',
-    options: {}
-  }
-};
--- src/index.ts
+++
@@ -1,9 +0,0 @@
-import SamorphicButton from './components/SamorphicButton.vue';
-
-export { SamorphicButton };
-
-export default {
-  install(app: any) {
-    app.component('SamorphicButton', SamorphicButton);
-  },
-};
